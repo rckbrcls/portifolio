159b2b04ece4717e30e72b99e19d3a15ec8742e0
brcls <petrick_barcelos@hotmail.com>
2024-06-29 12:45:18 -0300

    ajustando depedencias

--- apps/lojinha-simples/webpack.config.cjs
+++ apps/lojinha-simples/webpack.config.cjs
@@ -33,7 +33,11 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-react"],
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
           },
         },
       },
