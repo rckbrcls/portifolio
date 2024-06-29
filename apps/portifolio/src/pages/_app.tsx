ee38578ec99ecf06c45ef4fc28bc4196e52da563
brcls <petrick_barcelos@hotmail.com>
2024-06-29 15:02:59 -0300

    ajustado estilo

--- apps/portifolio/src/pages/_app.tsx
+++ apps/portifolio/src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Alert from "@/components/atoms/Alert";
 import Background from "@/components/atoms/Background/Background";
 import Header from "@/components/molecules/Header";
+import "./globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
