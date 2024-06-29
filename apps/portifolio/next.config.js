7f425886ecfdddd8178b9896a1bba06eed3ec26b
brcls <petrick_barcelos@hotmail.com>
2024-06-29 14:21:18 -0300

    alterando config de nextjs

--- apps/portifolio/next.config.js
+++ apps/portifolio/next.config.js
@@ -1,35 +1,18 @@
-const { ModuleFederationPlugin } = require("webpack").container;
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
 
 module.exports = {
-  webpack: (config, { isServer }) => {
-    config.experiments = {
-      topLevelAwait: true,
-      layers: true,
-    };
-
-    if (!isServer) {
-      config.output.publicPath = "http://localhost:3000/_next/";
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "host",
+          remotes: {
+            remote: "lojinha_simples@http://localhost:3001/bundle.js",
+          },
+          filename: "static/chunks/remoteEntry.js",
+        })
+      );
     }
-
-    config.plugins.push(
-      new ModuleFederationPlugin({
-        name: "shell",
-        remotes: {
-          lojinha_simples:
-            "lojinha_simples@http://localhost:3001/remoteEntry.js",
-        },
-        shared: {
-          react: {
-            singleton: true,
-            requiredVersion: "^18.2.0",
-          },
-          "react-dom": {
-            singleton: true,
-            requiredVersion: "^18.2.0",
-          },
-        },
-      })
-    );
 
     return config;
   },
