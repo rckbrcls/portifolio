0c7a5b646403ed5727e3d0ad330dad959d840fcc
brcls <petrick_barcelos@hotmail.com>
2024-06-30 20:07:28 -0300

    ajustando webpack

--- apps/alan-turing/src/components/ScrollButton.tsx
+++ apps/alan-turing/src/components/ScrollButton.tsx
@@ -1,4 +1,3 @@
-import { FaSolidArrowDownLong, FaSolidArrowUpLong } from "solid-icons/fa";
 import { Component } from "solid-js";
 
 interface IProps {
@@ -42,13 +41,23 @@ const ScrollButton: Component<IProps> = ({ mainRef }) => {
         onClick={scrollToPrevious}
         class="flex h-5 w-5 items-center justify-center gap-4 rounded-lg border border-zinc-800/50 bg-zinc-950 px-6 py-4 text-center font-bold"
       >
-        <FaSolidArrowUpLong />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path
+            fill="#ffffff"
+            d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"
+          />
+        </svg>
       </button>
       <button
         onClick={scrollToNext}
         class="flex h-5 w-5 items-center justify-center gap-4 rounded-lg border border-zinc-800/50 bg-zinc-950 px-6 py-4 text-center font-bold"
       >
-        <FaSolidArrowDownLong />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path
+            fill="#ffffff"
+            d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
+          />
+        </svg>
       </button>
     </div>
   );
