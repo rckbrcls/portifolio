1225b9d9a37cb69d85a3f6333c5503148e1257c6
brcls <petrick_barcelos@hotmail.com>
2024-08-21 16:21:58 -0300

    adicionado o projeto joystick

--- apps/portifolio/public/data/projects/projects.ts
+++ apps/portifolio/public/data/projects/projects.ts
@@ -1,21 +1,7 @@
-import TripperCapa from "../../images/projects/tripper/splashScreen.png";
-import PortifolioCapa from "../../images/projects/portifolio/capa.png";
 import { IProject } from "@/interface/IProject";
 import { tripperVisualization } from "./tripperVisualization";
 
 export const projects: IProject[] = [
-  {
-    slug: "portifolio",
-    description: `Crafted with Next.js and styled with Tailwind CSS, my personal portfolio serves as both a testing ground for
-                  frontend explorations and a showcase for my projects and accomplishments. Explore my work and journey
-                  through the interactive display of my skills and achievements.`,
-    name: "Portifolio",
-    techStack: ["React", "Next.js", "Tailwind", "TypeScript"],
-    timeline: null,
-    gitLink: "https://github.com/brcls/erick-barcelos",
-    members: ["Erick Barcelos"],
-    coverImage: PortifolioCapa,
-  },
   {
     slug: "tripper",
     description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
@@ -27,7 +13,7 @@ export const projects: IProject[] = [
     gitLink: "https://github.com/brcls/tripper",
     members: ["Erick Barcelos"],
     projectVisualization: tripperVisualization,
-    coverImage: TripperCapa,
+    coverImage: "/images/projects/tripper/splashScreen.png",
   },
   {
     slug: "alan-turing",
@@ -55,43 +41,41 @@ export const projects: IProject[] = [
     members: ["Erick Barcelos"],
     coverImage: "/images/projects/lojinha-simples/lojinha.png",
   },
-  // {
-  // slug: "dost",
-  //   description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
-  //                 landscapes, restaurants, or tourist attractions. Users have the ability to create and share their own routes, with
-  //                 the added feature of voting on routes to elevate their visibility within the community.`,
-  //   name: "DOST",
-  //   techStack: ["React", "TypeScript", "Tailwind", "Next.js"],
-  //   timeline: null,
-  //   link: "https://dostproject.vercel.app/",
-  //   members: ["Erick Barcelos"],
-  //   projectVisualization: tripperVisualization,
-  //   coverImage: TripperCapa,
-  // },
-  // {
-  //   slug: "joystick",
-  //   description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
-  //                 landscapes, restaurants, or tourist attractions. Users have the ability to create and share their own routes, with
-  //                 the added feature of voting on routes to elevate their visibility within the community.`,
-  //   name: "Joystick",
-  //   techStack: ["React", "JavaScript", "MongoDB", "Node.js"],
-  //   timeline: null,
-  //   gitLink: "https://github.com/brcls/joystick",
-  //   members: ["Erick Barcelos"],
-  //   projectVisualization: tripperVisualization,
-  //   coverImage: TripperCapa,
-  // },
-  // {
-  //   slug: "secret-santa",
-  //   description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
-  //                 landscapes, restaurants, or tourist attractions. Users have the ability to create and share their own routes, with
-  //                 the added feature of voting on routes to elevate their visibility within the community.`,
-  //   name: "Secret Santa",
-  //   techStack: ["React", "JavaScript", "MongoDB", "Node.js"],
-  //   timeline: null,
-  //   gitLink: "https://github.com/brcls/amigo-secreto",
-  //   members: ["Erick Barcelos"],
-  //   projectVisualization: tripperVisualization,
-  //   coverImage: TripperCapa,
-  // },
+  {
+    slug: "dost",
+    description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
+                  landscapes, restaurants, or tourist attractions. Users have the ability to create and share their own routes, with
+                  the added feature of voting on routes to elevate their visibility within the community.`,
+    name: "dost",
+    techStack: ["React", "TypeScript", "Tailwind", "Next.js"],
+    timeline: null,
+    link: "https://dostproject.vercel.app/",
+    members: ["Erick Barcelos"],
+    projectVisualization: tripperVisualization,
+    coverImage: "/images/projects/dost/dostHome.png",
+  },
+  {
+    slug: "joystick",
+    description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
+                  landscapes, restaurants, or tourist attractions. Users have the ability to create and share their own routes, with
+                  the added feature of voting on routes to elevate their visibility within the community.`,
+    name: "Joystick",
+    techStack: ["React", "JavaScript", "MongoDB", "Node.js"],
+    timeline: null,
+    gitLink: "https://github.com/brcls/joystick",
+    members: ["Erick Barcelos"],
+    coverImage: "/images/projects/lojinha-simples/lojinha.png",
+  },
+  {
+    slug: "secret-santa",
+    description: `Tripper is a React Native application designed to showcase routes for road trips that traverse scenic
+                  landscapes, restaurants, or tourist attractions. Users have the ability to create and share their own routes, with
+                  the added feature of voting on routes to elevate their visibility within the community.`,
+    name: "Secret Santa",
+    techStack: ["React", "JavaScript", "MongoDB", "Node.js"],
+    timeline: null,
+    gitLink: "https://github.com/brcls/secret-santa",
+    members: ["Erick Barcelos"],
+    coverImage: "/images/projects/lojinha-simples/lojinha.png",
+  },
 ];
