0cd1127b06c0283a6956952377f2d4a884e851f9
brcls <petrick_barcelos@hotmail.com>
2024-12-18 15:39:47 -0300

    Refactor Alert component to remove classNames dependency and inline styles for alert and button elements

--- apps/main/src/components/molecules/Alert.tsx
+++ apps/main/src/components/molecules/Alert.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from "react";
 import Text from "../atoms/Text";
-import classNames from "classnames";
 
 const Alert = () => {
   const [showAlert, setShowAlert] = useState(false);
@@ -17,26 +16,24 @@ const Alert = () => {
     setShowAlert(false);
   };
 
-  const alertContainerClass = classNames(
-    "p-4 z-50 glass-dark border-none fixed bottom-4 right-1/2",
-    "w-11/12 translate-x-1/2 flex md:flex-row flex-col justify-between text-start items-center",
-    "rounded-xl gap-4 transition duration-700 transform"
-  );
-
-  const buttonClass = classNames(
-    "py-2 px-4 rounded-full hover:scale-105 active:scale-95 transition duration-500",
-    "hover:bg-zinc-800 active:bg-zinc-900 border-zinc-100/20 border-2 md:w-min w-full"
-  );
-
   return (
     <>
       {showAlert && (
-        <div className={alertContainerClass}>
+        <div
+          className="p-4 z-50 glass-dark border-none fixed bottom-4 right-1/2 
+        w-11/12 translate-x-1/2 flex md:flex-row flex-col justify-between text-start items-center
+        rounded-xl gap-4 transition duration-700 transform"
+        >
           <Text>
             This portfolio is a work in progress, but rest assured, one day it
             will be a polished masterpiece! 😉
           </Text>
-          <button className={buttonClass} onClick={handleClose}>
+          <button
+            className="py-2 px-4 rounded-full hover:scale-105 active:scale-95 
+            transition duration-500 hover:bg-zinc-800 active:bg-zinc-900
+             border-zinc-100/20 border-2 md:w-min w-full"
+            onClick={handleClose}
+          >
             <Text>Close</Text>
           </button>
         </div>
