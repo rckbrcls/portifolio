69945ea1fb5fb908ba17f4d8e91724c8ba4d19f5
brcls <petrick_barcelos@hotmail.com>
2024-12-18 10:58:01 -0300

    Update Header component to use icons for navigation routes and adjust layout styles

--- apps/main/src/components/molecules/Header.tsx
+++ apps/main/src/components/molecules/Header.tsx
@@ -4,6 +4,10 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Text from "../atoms/Text";
 import { Route } from "next";
+import { PiHandWavingFill } from "react-icons/pi";
+import { MdComputer } from "react-icons/md";
+import { FaCircleInfo } from "react-icons/fa6";
+import { RiContactsBook2Fill } from "react-icons/ri";
 
 const Header = () => {
   const [background, setBackground] = useState(false);
@@ -37,30 +41,28 @@ const Header = () => {
   const headerClass = background ? "glass-dark border-none" : "bg-none";
 
   const buttonClass =
-    "py-1 px-6 rounded-full hover:scale-110 active:scale-95 duration-500 hover:bg-zinc-800 active:bg-zinc-900 flex items-center gap-2";
+    "p-2 rounded-full hover:scale-110 active:scale-95 duration-500 hover:bg-zinc-800 active:bg-zinc-900 flex items-center";
 
-  const headerStyle = `flex mt-4 justify-center items-center gap-4 px-4 py-2 w-min inset-x-0 
+  const headerStyle = `flex mt-4 justify-center items-center gap-10 px-4 py-2 w-min inset-x-0 
     mx-auto rounded-full fixed z-10 select-none transition duration-1000 ${headerClass}`;
 
   interface IRoute {
     path: Route;
-    label: string;
+    icon: React.ReactNode;
   }
 
   const routes: IRoute[] = [
-    { path: "/", label: "olá!" },
-    { path: "/projects", label: "projects" },
-    { path: "/about-me", label: "about me" },
-    { path: "/contact", label: "contact" },
+    { path: "/", icon: <PiHandWavingFill size={20} /> },
+    { path: "/projects", icon: <MdComputer size={20} /> },
+    { path: "/about-me", icon: <FaCircleInfo size={20} /> },
+    { path: "/contact", icon: <RiContactsBook2Fill size={20} /> },
   ];
 
   return (
     <header className={headerStyle}>
       {routes.map((route, index) => (
         <Link key={index} href={route.path}>
-          <button className={buttonClass}>
-            <Text className="whitespace-nowrap font-bold">{route.label}</Text>
-          </button>
+          <button className={buttonClass}>{route.icon}</button>
         </Link>
       ))}
     </header>
