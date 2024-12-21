c7098df6f4d39729250173c7c71ae5124a9035fc
brcls <petrick_barcelos@hotmail.com>
2024-12-21 18:10:43 -0300

    Update index.html title and enhance Webpack configuration with improved asset handling and development server settings

--- apps/lojinha-simples/webpack.config.cjs
+++ apps/lojinha-simples/webpack.config.cjs
@@ -1,8 +1,8 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ModuleFederationPlugin =
   require("webpack").container.ModuleFederationPlugin;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
@@ -10,79 +10,7 @@ module.exports = (env, argv) => {
   return {
     mode: isProduction ? "production" : "development",
     entry: "./src/main.tsx",
-    output: {
-      publicPath: isProduction ? "/" : "http://localhost:3001/",
-      path: path.resolve(__dirname, "dist"),
-      filename: isProduction ? "[name].[contenthash].js" : "bundle.js",
-      clean: true,
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js", ".jsx"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
-            "css-loader",
-            "postcss-loader",
-          ],
-        },
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                "@babel/preset-env",
-                "@babel/preset-react",
-                "@babel/preset-typescript",
-              ],
-            },
-          },
-        },
-        {
-          test: /\.tsx?$/,
-          use: {
-            loader: "ts-loader",
-            options: {
-              transpileOnly: true,
-            },
-          },
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "index.html"), // Corrigido o caminho do template
-      }),
-      new ModuleFederationPlugin({
-        name: "lojinha_simples",
-        library: { type: "var", name: "lojinha_simples" },
-        filename: "remoteEntry.js",
-        exposes: {
-          "./App": "./src/App",
-        },
-        shared: {
-          react: {
-            singleton: true,
-            requiredVersion: "^18.2.0",
-          },
-          "react-dom": {
-            singleton: true,
-            requiredVersion: "^18.2.0",
-          },
-        },
-      }),
-      ...(isProduction
-        ? [new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" })]
-        : []),
-    ],
-    devtool: isProduction ? "source-map" : "eval-source-map",
-    devServer: {
+    devServer: !isProduction && {
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods":
@@ -97,5 +25,100 @@ module.exports = (env, argv) => {
       hot: true,
       historyApiFallback: true,
     },
+    output: {
+      publicPath: isProduction ? "/" : "http://localhost:3001/",
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+    resolve: {
+      extensions: ["*", ".ts", ".tsx", ".js", ".jsx"],
+      extensionAlias: {
+        ".js": [".js", ".ts"],
+        ".cjs": [".cjs", ".cts"],
+        ".mjs": [".mjs", ".mts"],
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "postcss-loader",
+          ],
+        },
+        {
+          test: /\.(js|jsx|tsx|ts)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        },
+        {
+          test: /\.([cm]?ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+            },
+          },
+        },
+        {
+          test: /\.(png|jpg|gif|jpeg)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.svg$/i,
+          type: "asset",
+          resourceQuery: /url/, // *.svg?url
+        },
+        {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+          use: ["@svgr/webpack", "url-loader", "file-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Lojinha Simples",
+        filename: "index.html",
+        templateContent: `
+          <!DOCTYPE html>
+          <html lang="en">
+            <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>Lojinha Simples</title>
+              <link rel="stylesheet" href="main.css">
+            </head>
+            <body>
+              <div id="root"></div>
+            </body>
+          </html>
+        `,
+      }),
+      new MiniCssExtractPlugin({
+        filename: "main.css",
+      }),
+      new ModuleFederationPlugin({
+        name: "lojinha_simples",
+        library: { type: "var", name: "lojinha_simples" },
+        filename: "remoteEntry.js",
+        exposes: {
+          "./App": "./src/App",
+        },
+      }),
+    ],
   };
 };
