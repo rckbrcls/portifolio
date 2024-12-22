70dff9b005312afafd39990c3c84ef20a4d926ad
brcls <petrick_barcelos@hotmail.com>
2024-12-21 21:05:53 -0300

    Add Vercel Speed Insights integration and update dependencies

--- apps/main/src/pages/_app.tsx
+++ apps/main/src/pages/_app.tsx
@@ -2,11 +2,13 @@ import type { AppProps } from "next/app";
 
 import "./globals.css";
 import Aurora from "@/components/molecules/Aurora";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Aurora dark />
+      <SpeedInsights />
       <Component {...pageProps} />
     </>
   );
