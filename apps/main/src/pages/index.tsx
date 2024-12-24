247d9a36c82979c92fffe7cfbff3333be7c807c3
brcls <petrick_barcelos@hotmail.com>
2024-12-24 14:09:42 -0300

    Refactor Header and Aurora components; adjust z-index for improved layering, enhance Aurora positioning with dynamic classes, and update Home page layout for better structure and responsiveness

--- apps/main/src/pages/index.tsx
+++ apps/main/src/pages/index.tsx
@@ -5,7 +5,8 @@ import Title from "@/components/atoms/Title";
 import Header from "@/components/organisms/Header";
 import Alert from "@/components/molecules/Alert";
 import Head from "next/head";
-
+import { AnimatedBeamMultipleOutputDemo } from "@/components/organisms/AnimatedBeam";
+import SubTitle from "@/components/atoms/SubTitle";
 const MemoizedAurora = React.memo(Aurora);
 
 export default function Home() {
@@ -19,7 +20,7 @@ export default function Home() {
       <Alert />
       <Header />
       {aurora}
-      <main>
+      <main className="w-full">
         <div className="flex h-[100svh] flex-col items-center justify-center p-4 text-center">
           <div className="relative my-4 h-2/5 w-full select-none">
             <Image
@@ -33,6 +34,19 @@ export default function Home() {
             />
           </div>
           <Title word="Olá! I'm Erick Barcelos" />
+        </div>
+        <div className="grid h-screen w-full grid-cols-2 items-center justify-between max-md:grid-cols-1">
+          <AnimatedBeamMultipleOutputDemo />
+          <div className="flex flex-col items-start justify-start gap-10 p-14 text-start">
+            <Title word="Architecture" type="blur" />
+            <SubTitle>
+              My portfolio is crafted using a microfrontend architecture,
+              ensuring a modular, scalable, and easy-to-maintain structure. This
+              design allows seamless integration of various frameworks and
+              technologies, providing a smooth and consistent experience across
+              all projects.
+            </SubTitle>
+          </div>
         </div>
       </main>
     </>
