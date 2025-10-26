269bf26e857cabba9e7ae3b6bccc477ed18912f5
brcls <petrick_barcelos@hotmail.com>
2025-10-26 20:38:40 -0300

    refactor: update BoxGrid component styles for improved aesthetics and consistency; enhance About Me page layout for better structure

--- apps/main/src/pages/about-me/index.tsx
+++ apps/main/src/pages/about-me/index.tsx
@@ -19,43 +19,19 @@ export default function AboutMe() {
 
       <Aurora dark>
         <Header />
-        <div className="mx-auto flex w-full flex-col overflow-hidden pb-20 pt-24">
+        <div className="mx-auto flex w-full flex-col items-end overflow-hidden">
           {/* Hero Section */}
-          <div className="mb-10 w-full border-b border-zinc-700/30 pb-10">
-            <div className="mx-auto flex w-11/12 items-end justify-between gap-6 text-start max-sm:flex-col max-sm:text-center">
-              <div className="flex w-full flex-col items-end justify-between max-sm:items-center">
-                <motion.div
-                  initial={{ opacity: 0, y: 30 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.8 }}
-                >
-                  <Title word="About Me" type="blur" gradient />
-                </motion.div>
-
-                <motion.div
-                  initial={{ opacity: 0, y: 20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.6, delay: 0.2 }}
-                  className="max-w-2xl"
-                >
-                  <Text className="leading-relaxed"></Text>
-                  <Text className="leading-relaxed">
-                    Welcome! Here you'll find a bit about my journey, passions,
-                    and what inspires me beyond the code. Dive in and get to
-                    know the person behind the projects.
-                  </Text>
-                </motion.div>
-              </div>
-              {/* Resume Download Button */}
+          <div className="mb-5 w-full border-b border-zinc-700/30 pb-5">
+            <div className="mx-auto flex items-start justify-between gap-6 p-4 text-start max-sm:flex-col max-sm:text-center">
               <motion.div
                 initial={{ opacity: 0, y: 20 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.6, delay: 0.2 }}
                 viewport={{ once: true }}
-                className="flex justify-center max-sm:w-full"
+                className="absolute left-4 top-4 z-50 flex justify-center"
               >
                 <a
-                  className="glass-dark flex w-full items-center justify-center gap-3 text-nowrap rounded-lg px-8 py-4 text-base font-bold transition duration-300 hover:border-purple-400/40"
+                  className="glass-dark flex w-full items-center justify-center gap-3 text-nowrap rounded-3xl px-6 py-4 text-base font-bold transition duration-300 hover:border-purple-400/40"
                   href="/files/Resume.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -71,6 +47,34 @@ export default function AboutMe() {
                   />
                 </a>
               </motion.div>
+
+              <div className="flex w-full flex-col items-end justify-between max-sm:items-center">
+                <motion.div
+                  initial={{ opacity: 0, y: 30 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.8 }}
+                >
+                  <Title
+                    word="About Me"
+                    type="blur"
+                    gradient
+                    className="text-end"
+                  />
+                </motion.div>
+
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.6, delay: 0.2 }}
+                  className="max-w-2xl"
+                >
+                  <Text className="text-end leading-relaxed">
+                    Welcome! Here you'll find a bit about my journey, passions,
+                    and what inspires me beyond the code. Dive in and get to
+                    know the person behind the projects.
+                  </Text>
+                </motion.div>
+              </div>
             </div>
           </div>
 
@@ -79,7 +83,7 @@ export default function AboutMe() {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.2 }}
             viewport={{ once: true }}
-            className="mx-auto w-11/12"
+            className="w-11/12"
           >
             <BoxGrid />
           </motion.div>
